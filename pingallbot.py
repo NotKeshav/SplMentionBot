5ff35d30b364c0798d6aacbb0053d18da7f620a4
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-29 01:03:34 +0530

    Update pingallbot.py

--- pingallbot.py
+++ pingallbot.py
@@ -41,7 +41,7 @@ async def everyone(client, message):
         else:  
           chatQueue.append(message.chat.id)
           if len(message.command) > 1:
-            inputText = message.command[1]
+            inputText = message.text.split(None, 1)[1]
           elif len(message.command) == 1:
             inputText = ""    
           membersList = []
