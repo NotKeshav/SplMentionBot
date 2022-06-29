4741e470115298a3ace358f8ad0f7a0d8bb2ae47
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-29 17:13:26 +0530

    Update users.py

--- Database/users.py
+++ Database/users.py
@@ -71,3 +71,21 @@ def is_served_user(id):
         return True
     else:
         return False
+
+def clr_chats():
+    LEL = []
+    lel = SESSION.query(MS_users).all()
+        for lol in lel:
+            LEL.append(lol.chat_id)
+        for LOL in LEL:
+            omfoo = SESSION.query(MS_users).get(LOL)
+            SESSION.delete(omfoo)
+
+def clr_users():
+    LEL = []
+    lel = SESSION.query(M_users).all()
+        for lol in lel:
+            LEL.append(lol.id)
+        for LOL in LEL:
+            omfoo = SESSION.query(M_users).get(LOL)
+            SESSION.delete(omfoo)
