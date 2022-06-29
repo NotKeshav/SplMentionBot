4f5acb9c28f12be4e779184f8038812d9c543e06
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-29 09:50:54 +0530

    Update __init__.py

--- Database/__init__.py
+++ Database/__init__.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 
-DATABASE_URL = os.environ.get('DATABASE_URL', "")
+DATABASE_URL = os.environ.get('DATABASE_URL', None)
 
-DATABASE_URL = DATABASE_URL.replace("", "postgresql")
+DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
 
 
 def start() -> scoped_session:
