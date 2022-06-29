08eeeafe12d5df7bba4e557e95600d722059e18a
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-29 06:12:42 +0530

    Update pingallbot.py

--- pingallbot.py
+++ pingallbot.py
@@ -11,7 +11,7 @@ import asyncio
 from pyrogram import enums
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.errors import FloodWait
-
+from Database.users import *
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 START_MARKUP = [
