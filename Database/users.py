f807c5121ca903bd2f428825bef02ec3a58de7fb
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-29 06:15:18 +0530

    Update users.py

--- Database/users.py
+++ Database/users.py
@@ -11,9 +11,21 @@ class M_users(BASE):
     def __init__(self, id):
         self.id = id
 
+class MS_users(BASE):
+    __tablename__ = "msusers"
+
+    chat_id = Column(BigInteger, primary_key=True)
+
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+
 M_users.__table__.create(checkfirst=True)
 
+MS_users.__table__.create(checkfirst=True)
+
 User_IL = threading.RLock()
+
+LEL_IL = threading.RLock()
 
 def add_user(id):
     with User_IL:
@@ -30,3 +42,20 @@ def list_users():
         return lel
     except:
         SESSION.close()
+
+def add_chat(id):
+    with LEL_IL:
+        lel = SESSION.query(MS_users).get(id)
+        if not lel:
+            SESSION.add(MS_users(id))
+            SESSION.commit()
+        else:
+            SESSION.close()
+
+def list_users():
+    lel = SESSION.query(M_users).all()
+    try:
+        return lel
+    except:
+        SESSION.close()
+
