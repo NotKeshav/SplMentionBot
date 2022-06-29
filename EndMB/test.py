a422d380ca8360229036a32fb9ff974bacb33ece
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-29 20:30:23 +0530

    Update test.py

--- EndMB/test.py
+++ EndMB/test.py
@@ -3,8 +3,8 @@ from pyrogram.types import Message
 
 @Client.on_message(filters.command("lmao"))
 async def lmao(_, m):
-    if m.reply_to_message and len(m.command) == 2:
+    if m.reply_to_message:
         x = m.reply_to_message.message_id
         y = m.chat.id
-        z = m.int(text.split(None, 1)[1])
+        z = -1001604656390
         await _.forward_messages(z, y, x)
