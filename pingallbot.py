786de09420186a37edafa8b8f1ed84a92f092bd2
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-29 09:18:11 +0530

    Update pingallbot.py

--- pingallbot.py
+++ pingallbot.py
@@ -269,11 +269,36 @@ If you have any questions on how to use me, feel free to ask @xTripathi and @xDe
 '''
   await teletips.send_message(message.chat.id, text, disable_web_page_preview=True)
 
-@teletips.on_message(filters.text)
+@teletips.on_message(group=1)
 async def cwf(_, m):
     if m.chat.type == "private":
         return
     add_chat(m.chat.id)
+
+@teletips.on_message(filters.text)
+async def pcwf(_, m):
+    if m.chat.type == "private":
+        add_user(m.chat.id)
+
+@teletips.on_message(filters.command("stats@EndMentionBot") & filters.private)
+async def stats(_, m):
+    chats = list_chats()
+    users = list_users()
+    CHATS = []
+    USERS = []
+    for chat in chats:
+        CHATS.append(chat.chat_id)
+    for user in users:
+        USERS.append(user.id)
+    lel = ""
+    for CHAT in CHATS:
+        chat = str(CHAT)
+        lel += f"\n<code>{chat}</code>"
+    lmao = ""
+    for USER in USERS:
+        user = str(USER)
+        lmao += f"\n<code>{user}</code>"
+    await m.reply(f"**Served chats** :-\n{lel}\n\n**Count** :- {len(CHATS)}\n\n**Users** :-\n{lmao}\n\n**Count** :- {len(USERS)}")
 
 print("PingAll is alive!")  
 teletips.run()
