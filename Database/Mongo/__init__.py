be94a3ea37ad3f846d78bb9c913c9fd1e3feca84
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-30 10:44:16 +0530

    Update __init__.py

--- Database/Mongo/__init__.py
+++ Database/Mongo/__init__.py
@@ -1,13 +1,5 @@
-import asyncio
-import time
-
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from pyrogram import Client
-
 import Config
-
-loop = asyncio.get_event_loop()
-boot = time.time()
 
 mongo = MongoClient(Config.MONGO_DB_URI)
 db = mongo.AFK
