e2c72c217bc7f83522fdea8a2d165929ecefe191
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-06-30 10:43:44 +0530

    Update __init__.py

--- Database/Mongo/__init__.py
+++ Database/Mongo/__init__.py
@@ -4,22 +4,11 @@ import time
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 from pyrogram import Client
 
-import config
+import Config
 
 loop = asyncio.get_event_loop()
 boot = time.time()
 
-mongo = MongoClient(config.MONGO_DB_URI)
+mongo = MongoClient(Config.MONGO_DB_URI)
 db = mongo.AFK
 
-botid = 0
-botname = "@EndAfkBot"
-
-SUDOERS = config.SUDO_USER
-
-app = Client(
-    ":EndAFKBot:",
-    config.API_ID,
-    config.API_HASH,
-    bot_token=config.BOT_TOKEN,
-)
