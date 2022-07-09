357ba72c639775001118777a2f418901c5ce3be1
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-09 23:39:13 +0530

    Create owner.py

---
+++ EndMB/owner.py
@@ -0,0 +1,7 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+@Client.on_message(filters.command("alive"))
+async def hehe(_, m):
+    if m.from_user.id == 1927705508;
+        await m.reply("yes! Am alive owner 🤧")
