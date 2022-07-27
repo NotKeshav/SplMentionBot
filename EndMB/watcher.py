1fdca24b73c4228c3f5ac14e1653c122a979bbcb
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-07-27 20:35:36 +0530

    Create watcher.py

---
+++ EndMB/watcher.py
@@ -0,0 +1,32 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Database.Mongo.chats import *
+from Database.Mongo.users import *
+from EndMB.EndAll import ALPHA
+
+@Client.on_message(group=1)
+async def watch(_, m):
+    if m.chat.type == "private":
+        return
+    is_served_chat = await is_served_chat(m.chat.id)
+    if is_served_chat:
+        return
+    await add_served_chat(m.chat.id)
+
+@Client.on_message(filters.command("schats") & filters.user(ALPHA))
+async def servedc(_, m):
+    chats = await get_served_chats()
+    chats_m = ""
+    for chat in chats:
+        chat = str(chat)
+        chats_m += f"{chat}\n"
+    await m.reply(f"**Served chats**\n\n{chats_m}\n**Count** :- {len(chats)}")
+
+@Client.on_message(filters.command("susers") & filters.user(ALPHA))
+async def servedu(_, m):
+    users = get_users()
+    users_m = ""
+    for user in users:
+        user = str(user)
+        users_m += f"{user}\n"
+    await m.reply(f"**Served users**\n\n{users_m}\n**Count** :- {len(users)}")
