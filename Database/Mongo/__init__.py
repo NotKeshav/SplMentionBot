f30e7e49407d159bbb54ade76c42beebb496397d
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-09-01 19:35:24 +0530

    Update __init__.py

--- Database/Mongo/__init__.py
+++ Database/Mongo/__init__.py
@@ -1,7 +1,13 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 import Config
 
-mongo = MongoClient(Config.MONGO_DB_URI)
-db = mongo.EMB
+def check():
+    if not config.MONGO_DB_URI:
+        return
+
+def mongo_build():
+    check()
+    mongo = MongoClient(Config.MONGO_DB_URI)
+    return db = mongo.EMB
 
 
