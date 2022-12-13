3ca702d0b9234ed69827caf5868f1ba0e95e2342
Gokuvegeta928 <120127207+Gokuvegeta928@users.noreply.github.com>
2022-12-13 17:17:19 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,11 +1,11 @@
 import os
 
-API_ID = os.environ["API_ID"]
+API_ID = "13691707"
 
-API_HASH = os.environ["API_HASH"]
+API_HASH = "2a31b117896c5c7da27c74025aa602b8"
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
+BOT_TOKEN = "5873030820:AAEdD3K55SAoAWMPm7tnNR7uxd_Qp9Ov6sc"
 
 API_ID = int(API_ID)
 
-MONGO_DB_URI = os.environ["MONGO_DB_URI"]
+MONGO_DB_URI = "mongodb+srv://Lightyagami10:light@cluster0.zo6bf4g.mongodb.net/?retryWrites=true&w=majority"
